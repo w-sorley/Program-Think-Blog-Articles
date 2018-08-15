f181026cce710c902be599870d018040f86ee9c0
starrybird <star48b@gmail.com>
2018-08-15 20:15:26 +0800

    Add markdown articles generation script.

---
+++ program-think-md-gen.py
@@ -0,0 +1,154 @@
+#!/bin/bash/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+import re
+import shutil
+import requests
+import datetime
+from markdownify import markdownify as md
+
+
+def get_html(url):
+    return (requests.get(url).content).decode('utf-8')
+
+
+def get_article_title(html):
+    return ((html.split('<title>')[1]).split('</title>')[0]).split('@')[0]
+
+
+def get_core_content(html):
+    return (html.split("<div class='post-body entry-content'>")[1]).split("<div class='post-copyright'>")[0]
+
+
+def convert_html_to_md(html):
+    return md(html)
+
+
+def make_star_title_bigger(md_text):
+    return md_text.replace('★', '## ★')
+
+
+def write_markdown_file(file_name, md_text):
+    with open(file_name, 'w') as f:
+        f.write(md_text)
+
+
+def find_all_link(md_text):
+    name_regex = "[^]]+"
+    url_regex = "http[s]?://[^)]+"
+    markup_regex = '\\[({0})]\\(\\s*({1})\\s*\\)'.format(name_regex, url_regex)
+
+    return re.findall(markup_regex, md_text)
+
+
+def make_directory(directory, force_empty=False):
+    if force_empty and os.path.exists(directory) and os.path.isdir(directory):
+        shutil.rmtree(directory)
+    if not os.path.exists(directory):
+        os.system('mkdir -p ' + directory)
+
+
+def extract_image_name_from_link(link):
+    return (link.split('//')[1]).split('/')[1]
+
+
+def download_single_image(link, image_save_path):
+    r = requests.get(link, stream=True)
+
+    if r.status_code == 200:
+        with open(image_save_path + extract_image_name_from_link(link), 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+
+def download_all_image(image_save_path, matched_links_list):
+    make_directory(image_save_path)
+
+    for link in matched_links_list:
+        if link[0] == '不见图 请翻墙':
+            # print('Downloading image: %s' % link[1])
+            download_single_image(link[1], image_save_path)
+
+
+def replace_image_link_in_markdown_text(md_text, image_save_path, matched_links_list):
+    for link in matched_links_list:
+        if link[0] == '不见图 请翻墙':
+            # print('Replacing image link: %s' % link[1])
+            md_text = md_text.replace(link[1], image_save_path + extract_image_name_from_link(link[1]))
+    return md_text
+
+
+def get_md_name_from_url(url):
+    split_list = url.split('/')
+    return ((split_list[len(split_list) - 1]).split('.html')[0]) + '.md'
+
+
+def url_to_markdown(url):
+    print('Processing url: %s' % url)
+    image_save_path = "images/"
+    file_name = get_md_name_from_url(url)
+    html = get_html(url)
+    md_text = make_star_title_bigger(convert_html_to_md(get_core_content(html)))
+    matched_links_list = find_all_link(md_text)
+    download_all_image(image_save_path, matched_links_list)
+    md_text = replace_image_link_in_markdown_text(md_text, image_save_path, matched_links_list)
+    md_text = '# ' + get_article_title(html) + '\n\n-----\n\n' + md_text
+    write_markdown_file(file_name, md_text)
+
+
+def test_url_to_markdown():
+    url = 'https://program-think.blogspot.com/2018/08/USA-Containment-Strategies-in-Cold-War.html'
+    url_to_markdown(url)
+
+
+def find_article_links_in_url(url):
+    html = get_html(url)
+    pattern = re.compile(r"<h1 class='post-title entry-title'>\n<a href='(.*)'>")
+    match_list = pattern.findall(html)
+    return match_list
+
+
+def get_current_year_and_month():
+    now = datetime.datetime.now()
+    return now.year, now.month
+
+
+def construct_search_link(year, month):
+    return 'https://program-think.blogspot.com/%04d/%02d/' % (year, month)
+
+
+def download_all_articles_in_url_list(url_list):
+    for url in url_list:
+        url_to_markdown(url)
+
+
+def search_and_download_all_articles():
+    year = 2018
+    month = 6
+    current_year, current_month = get_current_year_and_month()
+
+    while year <= current_year:
+        year_dir = '%04d' % year
+        make_directory(year_dir, True)
+        os.chdir(year_dir)
+        while ((year != current_year and month <= 12) or (year == current_year and month <= current_month)):
+            print("Getting articles in %04d-%02d ......" % (year, month))
+            search_url = construct_search_link(year, month)
+            month_dir = '%02d' % month
+            make_directory(month_dir, True)
+            os.chdir(month_dir)
+            download_all_articles_in_url_list(find_article_links_in_url(search_url))
+            os.chdir('../')
+            month += 1
+        os.chdir('../')
+        month = 1
+        year += 1
+    print('Done !')
+
+
+if __name__ == '__main__':
+    # test_url_to_markdown()
+    # find_article_links_in_url('https://program-think.blogspot.com/2018/07/')
+    search_and_download_all_articles()
